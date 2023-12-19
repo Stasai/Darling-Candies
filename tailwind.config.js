2b7ca4318e6c8a1549c903041e1166441dcdd817
Stasai <nathanbobocea@gmail.com>
2023-12-18 17:13:16 -0800

    new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       backgroundImage: {
         'hero-image': "url('/build/img/candy-hero.jpg')",
       },
+      fontFamily : {
+        'mukta': ['Mukta', 'sans-serif']
+      },
     },
   },
   plugins: [],
