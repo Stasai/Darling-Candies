847706857a3ab785b561ed79a6e7009414e5b02d
Stasai <nathanbobocea@gmail.com>
2024-01-11 17:55:09 -0800

    autofill & autofit added to template cols

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
       fontFamily : {
         'mukta': ['Mukta', 'sans-serif']
       },
+      gridTemplateColumns: {
+        'auto-fill-100': 'repeat(auto-fill, minmax(100px, 1fr))',
+        'auto-fit-100': 'repeat(auto-fit, minmax(100px, 1fr))',
+      },
     },
   },
   plugins: [],
